6bf74484962801ac709a64728b5d3015e4a1dafc
nixitec <53209510+nixitec@users.noreply.github.com>
2019-09-18 13:48:51 +1000

    Update lungfi.sh

--- lungfi.sh
+++ lungfi.sh
@@ -1,1 +1,7 @@
-This is a bash file
+#!/bin/bash
+# Create a new user
+echo -e "Please enter the new user name: "
+read name
+adduser $name                   # Add the new user
+usermod -aG sudo $name          # Add the user to the sudo group
+echo "Finished adding user $name"
