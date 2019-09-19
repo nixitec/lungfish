8d22fc9ba8ee1630c8f59ff2a2e7c04885b229a3
nixitec <53209510+nixitec@users.noreply.github.com>
2019-09-19 17:12:28 +1000

    Update lungfi.sh

--- lungfi.sh
+++ lungfi.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-COL_BLUE="\x1b[32;01m"
+COL_GREEN="\x1b[32;01m"
 COL_RESET="\x1b[39;49;00m"
+
+MYPROJECT="nixography"
+MYPROJECT_ENV="nixographyenv"
 
 # Create a new user
 # echo -e "Please enter the new user name: "
@@ -9,9 +12,10 @@ COL_RESET="\x1b[39;49;00m"
 # usermod -aG sudo $name          # Add the user to the sudo group
 # echo "User $name added !"
 
-echo -e $COL_BLUE"Important Message: "$COL_RESET"This is a message"
-
+echo -e $COL_GREEN"Installing NGINX ..."$COL_RESET
 sudo apt install nginx
+
+echo -e $COL_GREEN"Enabling Firewall ..."$COL_RESET
 sudo ufw enable
 sudo ufw app list
 sudo ufw allow 'Nginx HTTP'
@@ -19,3 +23,14 @@ sudo ufw allow 'OpenSSH'
 sudo ufw status
 systemctl status nginx
 
+echo -e $COL_GREEN"Installing Python ..."$COL_RESET
+sudo apt install python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools
+
+echo -e $COL_GREEN"Creating Python venv ..."$COL_RESET
+sudo apt install python3-venv
+mkdir ~/$MYPROJECT
+cd ~/$MYPROJECT
+python3.6 -m venv $MYPROJECT_ENV
+source $MYPROJECT_ENV/bin/activate
+pip install wheel
+pip install gunicorn flask
