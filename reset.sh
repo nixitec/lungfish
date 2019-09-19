1b5b6f8ab22336978e5b9143398a74438106908b
nixitec <53209510+nixitec@users.noreply.github.com>
2019-09-20 08:24:57 +1000

    Update reset.sh

--- reset.sh
+++ reset.sh
@@ -4,7 +4,7 @@ sudo rm -rf nixography
 git clone https://github.com/nixitec/lungfish.git
 cd lungfish
 chmod +x lungfi.sh
-chmod +x reset.sh
 cp ./reset.sh ..
 cd ..
-sudo bash +x ./lungfi.sh
+chmod +x reset.sh
+sudo bash +x ./lungfish/lungfi.sh
