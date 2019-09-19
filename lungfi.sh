72212cd6b2fe0e7d9c24e356d19307437400b476
nixitec <53209510+nixitec@users.noreply.github.com>
2019-09-20 06:36:34 +1000

    Update lungfi.sh

--- lungfi.sh
+++ lungfi.sh
@@ -5,7 +5,7 @@ COL_RESET="\x1b[39;49;00m"
 
 MYPROJECT="nixography"
 MYPROJECT_ENV="nixographyenv"
-
+```
 echo -e $COL_GREEN"Installing NGINX, GreenUnicorn and Flask ..."$COL_RESET
 
 # Create a new user
@@ -28,13 +28,13 @@ sudo ufw allow 'OpenSSH'
 
 echo -e $COL_GREEN"Installing Python ..."$COL_RESET
 sudo apt install python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools
-
+```
 echo -e $COL_GREEN"Creating Python venv ..."$COL_RESET
 sudo apt install python3-venv
 mkdir ~/$MYPROJECT
 cd ~/$MYPROJECT
 pwd
 sudo python3.6 -m venv $MYPROJECT_ENV
-source $MYPROJECT_ENV/bin/activate
+source ~/$MYPROJECT/$MYPROJECT_ENV/bin/activate
 sudo pip install wheel
 sudo pip install gunicorn flask
