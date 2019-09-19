2fe57ef61fcf8ecdf734f5067d89208cee17a19f
nixitec <53209510+nixitec@users.noreply.github.com>
2019-09-20 08:33:27 +1000

    Update lungfi.sh

--- lungfi.sh
+++ lungfi.sh
@@ -50,7 +50,7 @@ source ./$MYPROJECT_ENV/bin/activate
 echo -e $COL_GREEN"Installing Wheel ..."$COL_RESET
 echo -e "Enter to continue: "
 read ok
-pip install wheel
+sudo -H pip install wheel
 
 echo -e $COL_GREEN"Installing Gunicorn and Flask ..."$COL_RESET
 echo -e "Enter to continue: "
