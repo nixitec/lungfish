16e25c596cf7e0e9439e77f70c0a2ee07e433525
nixitec <53209510+nixitec@users.noreply.github.com>
2019-09-19 16:26:30 +1000

    Update lungfi.sh

--- lungfi.sh
+++ lungfi.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-COL_BLUE="\x1b[34;01m"
+COL_BLUE="\x1b[32;01m"
 COL_RESET="\x1b[39;49;00m"
 
 # Create a new user
-echo -e "Please enter the new user name: "
-read name
-adduser $name                   # Add the new user
-usermod -aG sudo $name          # Add the user to the sudo group
-echo "User $name added !"
+# echo -e "Please enter the new user name: "
+# read name
+# adduser $name                   # Add the new user
+# usermod -aG sudo $name          # Add the user to the sudo group
+# echo "User $name added !"
 
 echo -e $COL_BLUE"Important Message: "$COL_RESET"This is a message"
 
